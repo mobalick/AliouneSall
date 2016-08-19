f574019d92fef63e0d1be82829b60398e54b347f
mobalick <mobalick@hotmail.com>
2016-08-19 05:01:09 +0200

    audiplayer object accessibility

--- app/pages/waxtaanu-fajar/waxtaanu-fajar.ts
+++ app/pages/waxtaanu-fajar/waxtaanu-fajar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AudioPlayer, AudioSource} from '../../providers/audio-player/audio-player'
 import {Observable} from 'rxjs/Observable';
@@ -10,11 +10,27 @@ import {AngularFire} from 'angularfire2';
 })
 export class WaxtaanuFajarPage {
   private audioSource: AudioSource;
+ @ViewChild(AudioPlayer) player: AudioPlayer;
   items: Observable<any[]>;
+  playList:any[];
+  
   constructor(private nav: NavController, af: AngularFire) {
     this.audioSource = new AudioSource("ChIbSakho 1", "https://firebasestorage.googleapis.com/v0/b/alioune-sall.appspot.com/o/Tafsir%2Ftafsir_04_07_2014.mp3?alt=media&token=4e3cae10-d799-4e3a-8887-a099ceb18da7", 22);
-    this.items        = af.database.list('/WaxtaanuFajar');
+    this.items       = af.database.list('/WaxtaanuFajar');
+    this.items.subscribe(values=>{
+      this.playList= values;
+    });
   }
 
+
+  ionViewDidEnter() {
+    var p = this.player;
+  }
+
+  play(src)
+  {
+    this.player.play(src);
+
+  }
 
 }
--- app/providers/audio-player/audio-player.ts
+++ app/providers/audio-player/audio-player.ts
@@ -1,13 +1,13 @@
 import { Injectable, Component , Input, } from '@angular/core';
 
 export class AudioSource {
-  name  : string;
+  title  : string;
   url   : string;
   id    : number;
   position : number;
   duration : number;
-  constructor(name: string, url:string, id:number) {
-    this.name     = name;
+  constructor(title: string, url:string, id:number) {
+    this.title    = title;
     this.url      = url;
     this.id       = id;
     this.position = 0;
@@ -17,47 +17,46 @@ export class AudioSource {
 @Component({
   selector: 'audio-player',
   template: `
-  <ion-card>
-    Name : {{source.name}} <br/>
-  </ion-card>
-
-        <div class="audio-player">
+        <div class="audio-player" *ngIf="isInitialized">
+          <ion-card>
+            Name : {{source.title}} <br/>
+          </ion-card>
 
           <ion-item *ngIf="state=='play'|| state=='pause' ">
-          
-            <ion-range [(ngModel)]="_progress" min="0" [max]="_duration" step="0.01" (ionChange)="seekTo($event)" >
-               <div range-left small >{{ formatTime(audio.currentTime) }}</div>
-               <div range-right small >{{ formatTime(audio.duration) }}</div>
+            <ion-range [(ngModel)]="_progress" min="0" [max]="_duration" step="0.01" (ionChange)="_seekTo($event)" >
+               <div range-left small >{{ _formatTime(audio.currentTime) }}</div>
+               <div range-right small >{{ _formatTime(audio.duration) }}</div>
             </ion-range>
           </ion-item>
+
           <ion-row>
             <ion-col>
-              <button primary clear right (click)="previousTrack()">
+              <button primary clear right (click)="_previousTrack()">
                 <ion-icon name="skip-backward"></ion-icon>
               </button>
             </ion-col>
 
             <ion-col *ngIf="!isPlaying" class="btn-center">
-              <button primary clear (click)="play()">
+              <button primary clear (click)="_play()">
                 <ion-icon name="play"></ion-icon>
               </button>
             </ion-col>
 
             <ion-col *ngIf="isPlaying" class="btn-center">
-              <button primary clear (click)="pause()">
+              <button primary clear (click)="_pause()">
                 <ion-icon name="pause" *ngIf="!isPaused"></ion-icon>
                 <ion-icon name="play" *ngIf="isPaused"></ion-icon>
               </button>
             </ion-col>
 
             <ion-col *ngIf="isPlaying" class="btn-center">
-              <button primary clear  (click)="stop()">
+              <button primary clear  (click)="_stop()">
                 <ion-icon name="square"></ion-icon>
               </button>
             </ion-col>
 
              <ion-col class="btn-end">
-              <button primary clear (click)="nextTrack()">
+              <button primary clear (click)="_nextTrack()">
                 <ion-icon name="skip-forward"></ion-icon>
               </button>
             </ion-col>
@@ -70,39 +69,41 @@ export class AudioSource {
 export class AudioPlayer {
   @Input()
   source          : AudioSource;
+  @Input()
   playList        : [AudioSource];
   isPlaying       : boolean;
   isPaused        : boolean;
   state           : string;
   audio           : HTMLAudioElement;
-  _progress : number;
-  _completed :number;
-  _duration : number;
+  isInitialized   : boolean
+  _progress       : number;
+  _completed      : number;
+  _duration       : number;
 
   constructor() {
     this.isPlaying=false;
    
   }
   
-  public previousTrack()
+  private _previousTrack()
   {
 
   }
  
-  public nextTrack()
+  private _nextTrack()
   {
     this._duration=2000;
   }
 
-  public play()
+  private _play()
   {
-    this.setPlayerState("play");
+    this._setPlayerState("play");
     this.audio.play();
   }
 
-  public pause()
+  private _pause()
   {
-    this.setPlayerState("pause");
+    this._setPlayerState("pause");
     if (this.audio.paused) {
       this.audio.play();
     }else {
@@ -110,20 +111,20 @@ export class AudioPlayer {
     }
   }
 
-  public stop()
+  private _stop()
   {
-    this.setPlayerState("stop");
+    this._setPlayerState("stop");
     this.audio.pause();
     this.audio.currentTime = 0;
     this._progress=0;
   }
  
-  public seekTo(ev) {
+  private _seekTo(ev) {
     this.audio.currentTime = ev.value;  
   }
 
 
- private setPlayerState(state : string)
+ private _setPlayerState(state : string)
  {
    this.state = state;
 
@@ -142,7 +143,7 @@ export class AudioPlayer {
    }
  }
 
-private formatTime(value:number) {
+ private _formatTime(value:number) {
   
     let s = Math.trunc(value % 60);
     let m = Math.trunc((value / 60) % 60);
@@ -150,31 +151,50 @@ private formatTime(value:number) {
     return h > 0 ? `${h<10?'0'+h:h}:${m<10?'0'+m:m}:${s<10?'0'+s:s}` : `${m<10?'0'+m:m}:${s<10?'0'+s:s}`;
   } 
 
-  private onTimeUpdate(e: Event) {
+  private _onTimeUpdate(e: Event) {
     if (this.isPlaying && this.audio.currentTime > 0) {
       this._progress = this.audio.currentTime;
       this._completed = this.audio.duration > 0 ? Math.trunc (this.audio.currentTime / this.audio.duration * 100)/100 : 0;
     }  
   }
 
-public getDuration = function (val) {
-    return new Date(0, 0, 0, 0, 0, val);
- };
-
   ngOnInit() {
-     this.audio = new Audio(this.source.url);
+    this._initAudio();
+  }
+
+  private _initAudio()
+  {
+    if(this.source==null && this.playList==null)
+      return;
+
+    if(this.source == null) 
+      this.source=this.playList[0]; 
+    
+    this.audio = new Audio(this.source.url);
 
     this.audio.addEventListener("durationchange", (e:any) => {    
       this._duration = e.target.duration;
     }, false);  
 
-    this.audio.addEventListener("timeupdate", (e) => { this.onTimeUpdate(e); }, false);
+    this.audio.addEventListener("timeupdate", (e) => { this._onTimeUpdate(e); }, false);
 
     this.audio.addEventListener("error", (err) => {
       console.log(`Audio error => track ${this.source.url}`, err);
       this.isPlaying = false;
     }, false);
+
+    this.isInitialized = true;
   }
 
+  public play(src)
+  {
+    if(this.isInitialized)
+      this._stop();
+
+    this.source = src;
+    this._initAudio();
+    
+    this._play()
+  }
 
 }
